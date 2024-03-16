ea289fb4b8c107c01af18cb13ff7c4533a4158ae
NCEK <101446019+ncek-XD@users.noreply.github.com>
2024-03-16 23:59:21 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo -e "\n[ installer tool for IGC -by NCEK DEV ]\n"
+echo -e "\n[wait] sedang menjalankan pkg update and pkg upgrade (📦) ...\n"
+pkg update -y && pkg upgrade -y
+echo -e "\n[info] sedang menginstall package dan dependencies ...\n"
+pkg install clang python binutils libffi openssl libsodium git -y
+echo -e "\n[info] cloning github repository -> 'IGC' ...\n"
+git clone https://github.com/ncek-XD/IGC && cd IGC
+echo -e "\n[info] proses install module and requirements ...\n"
+SODIUM_INSTALL=system pip3 install pynacl
+pip install wheel && pip install -r requirements.txt
+echo -e "\n[done] running 'run.py' to open IGC ...\n"
+python3 run.py
