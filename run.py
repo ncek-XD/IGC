3acd8033750c9e07734c4f86da1adbe94c46182d
NCEK <101446019+ncek-XD@users.noreply.github.com>
2024-03-17 20:41:55 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-import os, platform
+import os, platform, igc as ncekdev
 from os import path,system
 from platform import uname
 arch=uname().machine.lower()
 os.system('git pull')
 if 'aarch' in arch:
-	os.system('python .igc.so')
+	ncekdev.login_kamu()
 else:
-	os.system('python .igc.so')
+	ncekdev.login_kamu()
