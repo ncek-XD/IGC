a1bee38eb49af707e7f250e068b1d62d9df0eee9
NCEK <101446019+ncek-XD@users.noreply.github.com>
2024-05-09 18:18:16 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-import os, platform, igc as ncekdev
-from os import path,system
-from platform import uname
-arch=uname().machine.lower()
-os.system('git pull')
-if 'aarch' in arch:
-	ncekdev.login_kamu()
-else:
-	ncekdev.login_kamu()
+import os
+try: os.system("git pull")
+except: print("[!] anda memakai versi terbaru")
+os.system('./igc')
